052bef18c7f8e27e6be510d7d7f1f5753d0ff417
pRiMeXoMeGa <pRiMeXoMeGa@github.com>
2023-02-13 20:35:42 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { CategoryPageComponent } from './category-page/category-page.component'
+import { TodoslistPageComponent } from './todoslist-page/todoslist-page.component'
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'login', pathMatch: 'full' },
+  { path:'login', component:LoginFormComponent },
+  {path:'user', canActivate:[AuthGuard],
+    children: [
+              {
+                path: 'category',
+                component: CategoryPageComponent,
+                // canActivate:[AuthGuard]
+              },
+              {
+                path:'category/:category',
+                component: TodoslistPageComponent,
+                // canActivate:[AuthGuard]
+              }
+    ]
+  },
+  // {path:'todoslist', component:TodoslistPageComponent, canActivate:[AuthGuard]},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
